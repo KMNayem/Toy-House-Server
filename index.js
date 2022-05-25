540c7e3bed0049e54822862ba45ab7fbb68d6880
KMNayem <imna41695@gmail.com>
2022-05-25 13:41:49 +0600

    delete

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ async function run(){
             res.send(result);
         })
 
+        //DELETE
+        app.delete('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId()};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally{
 
